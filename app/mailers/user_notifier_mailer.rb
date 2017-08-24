d5ab9a41d1baab9e23ee845acb9b7900cb4b53a3
Vicente Retamal <vretamala@gmail.com>
2017-08-24 01:03:12 -0300

    Add Sengrid

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,12 @@
+class UserNotifierMailer < ApplicationMailer
+  default from: 'any_from_address@example.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_login_email(user, device, login)
+    @user = user
+    @login = login
+    @device = device
+    mail(to: @user.email,
+         subject: 'Nuevo inicio de sesión')
+  end
+end
